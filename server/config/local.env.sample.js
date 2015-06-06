0a477d2ba8c5baf0190f88396497c03b078b15bc
Brian Hedberg <bshslowfood@gmail.com>
2015-06-06 14:05:49 -0500

    Update local.env.sample.js

--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -9,14 +9,14 @@ module.exports = {
   DOMAIN:           'http://localhost:9000',
   SESSION_SECRET:   'realizechange-secret',
 
-  FACEBOOK_ID:      '1091820304167074',
-  FACEBOOK_SECRET:  '32f0ae25df47bac88c1d37315cb370ac',
+  FACEBOOK_ID:      'id',
+  FACEBOOK_SECRET:  'secret',
 
   TWITTER_ID:       'app-id',
   TWITTER_SECRET:   'secret',
 
-  GOOGLE_ID:        '896121692354-s1ril20ljt2r34s4bifht3rv68ear05p.apps.googleusercontent.com',
-  GOOGLE_SECRET:    'DPcFihaCiynWy5kTiZ2EX0T6',
+  GOOGLE_ID:        'id',
+  GOOGLE_SECRET:    'secret',
 
   // Control debug level for modules using visionmedia/debug
   DEBUG: ''
