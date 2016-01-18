3bd8dbff5c25d5dd1c87ccc73a5df9a33c95593e
Brian Hedberg <bshslowfood@gmail.com>
2016-01-18 01:06:46 -0700

    style city

--- client/app/routes/dreams/dreams.controller.js
+++ client/app/routes/dreams/dreams.controller.js
@@ -22,33 +22,51 @@ angular.module('realizeChangeApp')
     $scope.selectParam = function(param){
     	$scope.searchParam = param;
     };
-
-    $scope.resizeMap = {
-
+    // debugger
+    $scope.resizeMap = function () {
+      var newHeight = $window.innerHeight - 50
+      $('.map').height(newHeight)
     }
 
-
     $scope.addMarker = function(dream, index) {
-      console.log(dream, index)
-      var marker = L.marker([+dream.location.latitude, +dream.location.longitude]).addTo($scope.map);
-
-      var markerSettings = {
+      var coords = [+dream.location.latitude, +dream.location.longitude],
+        options = {
+          title: dream.future,
+          clickable: true
+        },
+        markerSettings = {
             "iconUrl": "http://www.wennemar.com/wp-content/uploads/2015/09/bernie-1.png",
             "iconSize": [20, 20],
             "iconAnchor": [10, 10],
             "popupAnchor": [0, -55],
             "className": "dot"
-        }
-      marker.setIcon(L.icon(markerSettings));
+        },
+        markerOpacity = .5;
+
+      var marker = L.marker(coords, options).addTo($scope.map)
+        // .bindPopup(dream.future)
+        .setIcon(L.icon(markerSettings))
+        .setOpacity(markerOpacity);
+        
+      // marker.on('click', function(e) {
+      //   // e.target.openPopup()
+      // });
+
+      // marker._leaflet_id;
 
     }
   
   // Provide your access token
   L.mapbox.accessToken = 'pk.eyJ1IjoiZ2VtZmFybWVyIiwiYSI6ImtNWkpLbHcifQ.jWx398eYRGPYROgOcxXjdQ';
 
-
-
+   // initialize map
   $scope.map = L.mapbox.map('map', 'gemfarmer.4df00baa');
+  $scope.resizeMap();
+
+
+  angular.element($window).bind('resize', function(){
+     $scope.resizeMap();
+   });
 
 
   }]);
